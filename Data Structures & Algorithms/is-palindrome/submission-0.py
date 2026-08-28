30c4db804a0897ec2ebaf4d0ee1a327b8d791294
annampaul <annampaul000@gmail.com>
2026-08-28 19:49:34 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        processed="".join(
+            ch.lower()
+            for ch in s
+                if ch.isalnum()
+        )
+        return processed == processed[::-1]
+        
